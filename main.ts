f7ff5a173ceea12549043c7451131a58ce2ffcd2
mashutan <67031828+mashutan@users.noreply.github.com>
2020-06-17 16:06:48 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,30 +6,31 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.showIcon(IconNames.Triangle)
     }
     if (receivedNumber == 0 && rerecenumber == 1 || receivedNumber == 1 && rerecenumber == 2 || receivedNumber == 2 && rerecenumber == 0) {
-        basic.showIcon(IconNames.Sad)
+        basic.showIcon(IconNames.No)
     }
     if (receivedNumber == 0 && rerecenumber == 2 || receivedNumber == 1 && rerecenumber == 0 || receivedNumber == 2 && rerecenumber == 1) {
-        basic.showIcon(IconNames.Happy)
+        basic.showIcon(IconNames.SmallHeart)
     }
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Diamond)
-    basic.pause(5000)
+    basic.pause(2000)
     radio.sendNumber(0)
     rerecenumber = 0
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Heart)
+    rerecenumber = 3
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Square)
-    basic.pause(5000)
+    basic.pause(2000)
     radio.sendNumber(2)
     rerecenumber = 2
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Scissors)
-    basic.pause(5000)
+    basic.pause(2000)
     radio.sendNumber(1)
     rerecenumber = 1
 })
